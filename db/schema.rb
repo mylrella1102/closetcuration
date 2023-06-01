54e64e35434c3f6e65f38639c1c2ec55496f8aaf
mylrella1102 <tmk12.3u@gmail.com>
2023-06-01 11:43:08 +0900

    サイドバーの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_31_071058) do
+ActiveRecord::Schema.define(version: 2023_05_31_142439) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "weather_id"
+    t.integer "temp_low_id"
+    t.integer "temp_high_id"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +37,5 @@ ActiveRecord::Schema.define(version: 2023_05_31_071058) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
