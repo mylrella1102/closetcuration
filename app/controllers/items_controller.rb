14dbf3b88c6b0ad4cb904270c0a06739062deb74
mylrella1102 <tmk12.3u@gmail.com>
2023-06-08 21:35:25 +0900

    更新機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,6 +28,15 @@ class ItemsController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
   
   private
 
