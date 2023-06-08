3f4347a7dcc5993e877310e4bb8f3a3520b5c854
mylrella1102 <tmk12.3u@gmail.com>
2023-06-08 20:02:44 +0900

    ルートパスの変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @items = Item.all
     @posts = Post.all.order('created_at DESC')
   end
 
