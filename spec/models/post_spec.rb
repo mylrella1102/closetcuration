255fb418427a9d03f2f5ef35c69b1e5007d61584
mylrella1102 <tmk12.3u@gmail.com>
2023-07-04 01:31:46 +0900

    postテスト

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe 'コーディネート投稿機能' do
+    context '投稿できる場合' do
+      it 'date,weather_id,low_id,high_id,content,user,accountが存在すれば登録できる' do
+        expect(@post).to be_valid
+      end
+    end
+    context '投稿できない場合' do
+      it 'dateが空では保存できない' do
+        @post.date = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Date can't be blank")
+      end
+
+      it 'userが紐付いていないと保存できない' do
+        @post.user = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include('User must exist')
+      end
+
+      it 'accountが紐付いていないと保存できない' do
+        @post.account = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include('Account must exist')
+      end
+
+    end
+  end
 end
