5ccaf796f96170510687deddb638d1ba36ff27c1
mylrella1102 <tmk12.3u@gmail.com>
2023-07-08 00:56:12 +0900

    accountテスト

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,4 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
+  before do
+    @account = FactoryBot.build(:account)
+  end
+
+  describe 'アイテム登録機能' do
+    context '登録できる場合' do
+      it 'name,date,userが存在すれば登録できる' do
+        expect(@account).to be_valid
+      end
+    end
+    context '登録できない場合' do
+      it 'nameが空では保存できない' do
+        @account.name = ''
+        @account.valid?
+        expect(@account.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'userが紐付いていないと保存できない' do
+        @account.user = nil
+        @account.valid?
+        expect(@account.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
